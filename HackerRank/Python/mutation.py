15565bce0507c1d7d05c855149dca9c597ca1e12
SaifSohad <sohad.cse@gmail.com>
2021-02-27 16:31:43 +0600

    python

--- HackerRank/Python/mutation.py
+++ HackerRank/Python/mutation.py
@@ -1,11 +1,14 @@
-def mutate_string(string, position, character):
-    l=list(string)
-    l[position]=character
-    l_new="".join(l)
-    return l_new
+def count_substring(string, sub_string):
+    count=0
+    for i in range(len(string)-len(sub_string)+1):
+      if string[i:i+len(sub_string)]==sub_string :
+        count += 1
+
+    return count
 
 if __name__ == '__main__':
-    s = input()
-    i, c = input().split()
-    s_new = mutate_string(s, int(i), c)
-    print(s_new)
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
